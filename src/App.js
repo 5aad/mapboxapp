35b85ba1838da3a9ae488d0a6d8efea98952a0cb
Saad <saad_anwer@live.com>
2020-04-28 20:53:09 +0500

    init

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import React from 'react';
 import mapboxgl from 'mapbox-gl';
 import './App.css';
 
+//env sai is mai token ka karna
 mapboxgl.accessToken = 'pk.eyJ1IjoiNWFhZCIsImEiOiJjazlqeGo4YzUwMnhoM2pxYWQ3Z3gyMW1vIn0.FVlTmXS_ksLqzGwz9VTCdQ';
-
+//https://docs.mapbox.com/mapbox-gl-js/example/setstyle/
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
     lng: 5,
     lat: 34,
-    zoom: 2
+    //Zoom ki value change karana hai 
+//     0	The Earth
+// 3	A continent
+// 4	Large islands
+// 6	Large rivers
+// 10	Large roads
+// 15	Buildings
+    zoom: 2,
+    //is ki state change karni hai 
+    mapStyle:"streets-v11"
     };
     }
+
+
      
     componentDidMount() {
     const map = new mapboxgl.Map({
     container: this.mapContainer,
-    style: 'mapbox://styles/mapbox/streets-v11',
+    //satellite-v9
+    //light-v10
+    style: ('mapbox://styles/mapbox/'+ this.state.mapStyle),
     center: [this.state.lng, this.state.lat],
     zoom: this.state.zoom
     });
@@ -38,9 +52,11 @@ class App extends React.Component {
     </div>
     <div className="buttonStyle">
     <div class="btn-group" role="group" aria-label="Basic example">
-  <button type="button" class="btn btn-secondary">Style1</button>
-  <button type="button" class="btn btn-secondary">Style2</button>
-  <button type="button" class="btn btn-secondary">Style3</button>
+  <button   type="button" class="btn btn-secondary">streets</button>
+  <button  type="button" class="btn btn-secondary">satellite</button>
+  <button   type="button" class="btn btn-secondary">Light</button>
+  <button  type="button" class="btn btn-secondary"> +</button>
+  <button   type="button" class="btn btn-secondary"> -</button>
 </div>
 </div>
     <div ref={el => this.mapContainer = el} className='mapContainer' />
