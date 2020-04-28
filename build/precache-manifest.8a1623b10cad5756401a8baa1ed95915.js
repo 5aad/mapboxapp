68a6bd128fc381c7931b3503c8aed5a1b8b38ca7
Saad <saad_anwer@live.com>
2020-04-29 01:55:17 +0500

    init d

---
+++ build/precache-manifest.8a1623b10cad5756401a8baa1ed95915.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81cc3368690571c7acd4ae219b0c1c56",
+    "url": "/mapboxapp/index.html"
+  },
+  {
+    "revision": "36678fa06ef8ba82c500",
+    "url": "/mapboxapp/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "553a4ec898eb412aedb5",
+    "url": "/mapboxapp/static/css/main.d70bbebd.chunk.css"
+  },
+  {
+    "revision": "36678fa06ef8ba82c500",
+    "url": "/mapboxapp/static/js/2.a8749fcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mapboxapp/static/js/2.a8749fcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553a4ec898eb412aedb5",
+    "url": "/mapboxapp/static/js/main.67629485.chunk.js"
+  },
+  {
+    "revision": "26968a650dcd70d021d0",
+    "url": "/mapboxapp/static/js/runtime-main.c36e5738.js"
+  }
+]);
