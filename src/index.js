c0826b82b38e38573472589d304d57aeff0ff0bc
Saad <saad_anwer@live.com>
2020-04-29 10:20:35 +0500

    final

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Map from './MapBox/Map';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Map />
   </React.StrictMode>,
   document.getElementById('root')
 );
