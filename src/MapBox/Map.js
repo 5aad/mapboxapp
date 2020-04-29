432acd7256d26c1e731afc25ab14fbdedb3a1a83
Saad <saad_anwer@live.com>
2020-04-29 13:57:56 +0500

    finish

--- src/MapBox/Map.js
+++ src/MapBox/Map.js
@@ -6,99 +6,94 @@ import { MAPBOX_KEY } from "./MapboxAPIkey";
 
 mapboxgl.accessToken = MAPBOX_KEY;
 class Map extends React.Component {
+    mapRef = React.createRef();
+    map;
+  
     constructor(props) {
-        super(props);
-        this.state = {
-        lng: 5,
-        lat: 34,
-        zoom: 2, 
-    
-        mapStyle:"streets-v11"
-        };
-        this.handleStyle=this.handleStyle.bind(this)
-        }
-
-        //Custom Zoom Logic
-        handleIncrement=()=>{
-          let zo = parseFloat(this.state.zoom)
-          
-          if(zo<parseFloat(22))
-              this.setState({
-                zoom:zo+parseFloat(1)
-              })
-        }
-        handleDecrement=()=>{
-          let zo = parseFloat(this.state.zoom)
-          if(zo>parseFloat(2))
-              this.setState({
-                zoom:zo-parseFloat(1)
-              })
-        }
+      super(props);
+      this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 2,
+      //is ki state change karni hai 
+  
+      mapStyle:"streets-v11"
+      };
+      this.handleStyle=this.handleStyle.bind(this)
+      }
+    //   handleIncrement=()=>{
+    //     let zo = parseFloat(this.state.zoom)
         
-
-    
-        componentDidUpdate() {
-          console.log('update')
-        const map = new mapboxgl.Map({
-        container: this.mapContainer,
-        //satellite-v9
-        //light-v10
-        style: ('mapbox://styles/mapbox/'+ this.state.mapStyle),
-        center: [this.state.lng, this.state.lat],
-        zoom: this.state.zoom
-        });
+    //     if(zo<parseFloat(22))
+    //         this.setState({
+    //           zoom:zo+parseFloat(1)
+    //         })
+    //   }
+    //   handleDecrement=()=>{
+    //     let zo = parseFloat(this.state.zoom)
+    //     if(zo>parseFloat(2))
+    //         this.setState({
+    //           zoom:zo-parseFloat(1)
+    //         })
+    //   }
+      
+  
+       componentDidUpdate() {
+        this.setstyl()
         
-        //default zoom
-        // const nav = new mapboxgl.NavigationControl()
-        // map.addControl(nav)
+      } 
+      setstyl=()=>{
+        const style=this.state.mapStyle
+        console.log(style)
+        this.map.setStyle('mapbox://styles/mapbox/'+ style)
         
-        map.on('move', () => {
-        this.setState({
-        lng: map.getCenter().lng.toFixed(4),
-        lat: map.getCenter().lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
-        });
-        });
-        }
+      }
+      // shouldComponentUpdate(nextProps,nextState){
         
-        shouldComponentUpdate(nextProps,nextState){
-          
-          if( nextState.zoom!==this.state.zoom)
-              return true
-          if(nextState.mapStyle!==this.state.mapStyle){
-            return true
-          }
-          return false
-        }
-        componentDidMount(){
-          const map = new mapboxgl.Map({
+      //   if(nextState.mapStyle!==this.state.mapStyle){
+      //     return true
+      //   }
+      //   return false
+      // }
+      componentDidMount(){
+         this.map = new mapboxgl.Map({
+          container: this.mapContainer,
+          //satellite-v9
+          //light-v10
+          style: ('mapbox://styles/mapbox/'+ this.state.mapStyle),
+          center: [this.state.lng, this.state.lat],
+          zoom: this.state.zoom
+          });
+  
+          const nav = new mapboxgl.NavigationControl({
             container: this.mapContainer,
-            //satellite-v9
-            //light-v10
-            style: ('mapbox://styles/mapbox/'+ this.state.mapStyle),
-            center: [this.state.lng, this.state.lat],
-            zoom: this.state.zoom
-            });
-             
-            map.on('move', () => {
-            this.setState({
-            lng: map.getCenter().lng.toFixed(4),
-            lat: map.getCenter().lat.toFixed(4),
-            zoom: map.getZoom().toFixed(2)
-
-            });
-            });
-        //default zoom
-        // const nav = new mapboxgl.NavigationControl()
-        // map.addControl(nav)
-        }
-     
-        handleStyle=(e)=>{
-            this.setState({
-              mapStyle:e.target.value
-            })
+        })
+        this.map.addControl(nav)
            
-        }
+        this.map.on('move', () => {
+          this.setState({
+          lng: this.map.getCenter().lng.toFixed(4),
+          lat: this.map.getCenter().lat.toFixed(4),
+          zoom: this.map.getZoom().toFixed(2)
+          });
+          });
+          this.map.on('click', () => {
+            this.setState({
+            lng: this.map.getCenter().lng.toFixed(4),
+            lat: this.map.getCenter().lat.toFixed(4),
+            zoom: this.map.getZoom().toFixed(2)
+            });
+            });
+          this.setstyl()
+      }
+   
+      handleStyle=(e)=>{
+         console.log(e.target.value)
+          this.setState({
+            mapStyle:e.target.value
+          })
+         
+      }
   render() {
     return (
       <div>
@@ -115,8 +110,8 @@ class Map extends React.Component {
       <button   type="button" className="btn btn-secondary" value="light-v10" onClick={(e)=>this.handleStyle(e)}>Light</button>
      
       {/* Custom Zoom buttons */}
-      <button  type="button" className="btn btn-secondary" onClick={this.handleIncrement}> +</button>
-      <button   type="button" className="btn btn-secondary" onClick={this.handleDecrement}> -</button>
+      {/* <button  type="button" className="btn btn-secondary" onClick={this.handleIncrement}> +</button>
+      <button   type="button" className="btn btn-secondary" onClick={this.handleDecrement}> -</button> */}
           </div>
         </div>
         <div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
